4c84aecaf3741567a2913a3d24f8404c3d357d57
Nileesha Fernando <nileeshasajani@hotmail.com>
2019-02-18 21:12:30 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="script.js"></script>
+	<title>Widget</title>
+</head>
+<body>
+	<h1>Ratings Widget</h1>
+    
+    Enter item id:
+    <input id= "itemId" name="id" type ="text"/>
+	<input id = "submit" type="submit" value="submit">
+
+	<h2>Results:</h2>
+	<div id = "ratings-widget"></div>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,66 @@
+//Reference: http://alexmarandon.com/articles/web_widget_jquery/
+
+(function() {
+
+var jQuery;
+
+if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.4.2') {
+    var script_tag = document.createElement('script');
+    script_tag.setAttribute("type","text/javascript");
+    script_tag.setAttribute("src",
+        "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+    if (script_tag.readyState) {
+      script_tag.onreadystatechange = function () { // For old versions of IE
+          if (this.readyState == 'complete' || this.readyState == 'loaded') {
+              scriptLoadHandler();
+          }
+      };
+    } else { 
+      script_tag.onload = scriptLoadHandler;
+    }
+    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+} else {
+    jQuery = window.jQuery;
+    main();
+}
+
+/******** Called once jQuery has loaded ******/
+function scriptLoadHandler() {
+    // Restore $ and window.jQuery to their previous values and store the
+    // new jQuery in our local jQuery variable
+    jQuery = window.jQuery.noConflict(true);
+    // Call our main function
+    main(); 
+}
+
+/******** Our main function ********/
+function main() { 
+    jQuery(document).ready(function($) { 
+    	$("#submit").click(function(e) {
+   			e.preventDefault();
+			var id = $("#itemId").val();
+			var url = 'http://localhost:3000/api/ratingaverage/json?id='+id;
+
+			$.ajax( {
+		    	url: url,
+		    	dataType: 'json',
+		    	type: 'GET',
+	
+		   		success: function(response) {
+		       		var ratingAverage = '<p> id: ' + response.id + '</p>';
+		       		ratingAverage += '<p>name: ' + response.name + '</p>';
+		       		ratingAverage += '<p>average: ' + response.average + '</p>';
+		       		ratingAverage += '<p>ratings Count: ' + response.ratingsCount + '</p>';
+
+		       		$('#ratings-widget').html(ratingAverage);
+		       
+		   		},
+		    	error: function(error){
+		       		$('#ratings-widget').html('<p>Item not found.</p>');
+		    	}
+			});
+    	});
+    });
+}
+
+})(); // We call our anonymous function immediately
